91ffba31e3ac5a321c96f7b948e3a7a654daf7b0
Pratik Neupane <your_email@example.com>
2025-01-05 21:56:54 +0545

    bold button event listner added

--- public/scripts/descriptionBox.js
+++ public/scripts/descriptionBox.js
@@ -2,4 +2,12 @@ document.addEventListener("DOMContentLoaded",() => {
 
     const boldbutton = document.getElementById("bold");
     const italicbutton = document.getElementById("italic");
+
+    console.log("pratik");
+
+
+    boldbutton.addEventListener("click", () =>{
+        document.execCommand("bold",false,null);
+    })
+    
 })
