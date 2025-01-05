51ffd16b1d26c9d097efc8d7c6fd142598b5ea82
Pratik Neupane <your_email@example.com>
2025-01-05 21:58:28 +0545

    italic button event listner added

--- public/scripts/descriptionBox.js
+++ public/scripts/descriptionBox.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded",() => {
     const boldbutton = document.getElementById("bold");
     const italicbutton = document.getElementById("italic");
 
-    console.log("pratik");
+    console.log("true");
 
 
     boldbutton.addEventListener("click", () =>{
         document.execCommand("bold",false,null);
-    })
+    });
+
+    italicbutton.addEventListener("click", () =>{
+        document.execCommand("italic",false,null);
+    });
     
 })
