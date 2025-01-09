817f1e83255cd66b2bb3de492b05e2bbe827174d
npratik0 <neupanepratik00@gmail.com>
2025-01-09 21:08:10 +0545

    EventListner added

---
+++ public/scripts/descriptionBox.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const boldbutton = document.getElementById("bold");
+    const italicbutton = document.getElementById("italic");
+
+    console.log("pratik");
+})
