e6f676208f12c6f2d139788895e3add6b4ace355
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2025-01-21 21:09:06 +0545

    create user model and /signup post route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const database = require('./data/database')
+const User = require('./models/User')
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.render('landing');
 })
@@ -16,6 +19,10 @@ app.get('/', (req, res) => {
 app.get('/signup', (req, res) => {
     res.render('signup');
 })
+
+app.post('/signup', (req, res)=> {
+    const user = new User(req.body.username, req.body.contactNo, req.body.email, req.body.password, req.body.city);
+});
 
 app.get('/login', (req, res) => {
     res.render('login');
---
+++ models/User.js
@@ -0,0 +1,23 @@
+class User {
+    constructor(name, contactno, email, password, city) {
+        this.name = name;
+        this.contactno = contactno;
+        this.email = email;
+        this.password = password;
+        this.city = city;
+    }
+
+    insertUser() {
+
+    }
+
+    getLogin() {
+
+    }
+
+    updateUser() {
+
+    }
+}
+
+module.exports = User;
