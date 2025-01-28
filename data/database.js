80c8ebf64d0c053e2ebda57e97892dcb40ebe8c3
npratik0 <neupanepratik00@gmail.com>
2025-01-28 16:45:31 +0545

    Signup style changed

--- data/database.js
+++ data/database.js
@@ -4,7 +4,7 @@ const { Client } = pg;
 
 const client = new Client({
   user: "postgres",
-  password: "123#@!456^%$789(*&",
+  password: "12345",
   host: "localhost",
   port: 5432,
   database: "postgres",
@@ -12,6 +12,7 @@ const client = new Client({
 
 async function connectToDatabase() {
     const res = await client.connect();
+    console.log("Success");
 }
 
 
